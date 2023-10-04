080fa58f2cb5576a9cdd93f5aff069b607a2a3f6
Laaw05 <dhoanglam79@gmail.com>
2023-10-04 16:43:46 +0700

    fixbuoi1

--- 63CNTT4N2HL/63CNTT4N2HL/Areas/Admin/AdminAreaRegistration.cs
+++ 63CNTT4N2HL/63CNTT4N2HL/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace _63CNTT4N2HL.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new {controller="Admin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
